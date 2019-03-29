5f2102b953949a2a0bf25738de2c7ce6573da8fe
Julia Salnikova (Notiel) <agereth@gmail.com>
2019-03-29 19:50:18 +0300

    tests rewritten

--- tests.py
+++ tests.py
@@ -1,66 +1,69 @@
 import questiondata
 import category_rewritten
+import unittest
 
 
-def test_file_open():
-    game, error = questiondata.create_game("Example.xlsx")
-    if not game:
-        print("Open file test failed: " + error)
-        return
-    if game.length != 2 or len(game.categories) != 2:
-        print('open file test failed, wrong category number')
-        return
-    category = game.get_category_by_name('Красивые картинки')
-    if not category:
-        print('open file test failed, category is absent')
-        return
-    if len(category.questions) != 2:
-        print('open file failed, wrong questions')
-        return
-    total = 0
-    total_points = 0
-    for category in game.categories:
-        total += category.get_question_number()
-        total_points += category.get_total_points()
-    if total != 4:
-        print('open file test: wrong questions number')
-        return
-    if total_points != 60:
-        print('open file test: wrong points')
-        return
-    print('Open file test: passed')
+class TestGameOpened(unittest.TestCase):
+
+    def test_GameOpened(self):
+        game, _ = questiondata.create_game("Example.xlsx")
+        self.assertIsNotNone(game)
+
+    def test_GameLen(self):
+        game, _ = questiondata.create_game("Example.xlsx")
+        self.assertEqual(2, game.length)
+
+    def test_CategoryLen(self):
+        game, _ = questiondata.create_game("Example.xlsx")
+        self.assertEqual(2, len(game.categories))
+
+    def test_Category(self):
+        game, _ = questiondata.create_game("Example.xlsx")
+        category = game.get_category_by_name('Красивые картинки')
+        self.assertIsNotNone(category)
+
+    def test_CategoryQuestionLen(self):
+        game, _ = questiondata.create_game("Example.xlsx")
+        category = game.get_category_by_name('Красивые картинки')
+        self.assertEqual(2, len(category.questions))
+
+    def test_CategoryQuestionNumber(self):
+        game, _ = questiondata.create_game("Example.xlsx")
+        total = 0
+        for category in game.categories:
+            total += category.get_question_number()
+        self.assertEqual(4, total)
+
+    def test_CategoryQuestionPoints(self):
+        game, _ = questiondata.create_game("Example.xlsx")
+        total_points = 0
+        for category in game.categories:
+            total_points += category.get_total_points()
+        self.assertEqual(60, total_points)
 
 
-def test_get_size():
-    x, y = category_rewritten.get_size_data(36)
-    if x != 6 or y != 6:
-        print("Get  size failed for 36")
-        return
-    x, y = category_rewritten.get_size_data(35)
-    if x != 7 or y != 5:
-        print("Get  size failed for 35")
-        return
-    x, y = category_rewritten.get_size_data(21)
-    if x != 5 or y != 5:
-        print("Get  size failed for 21")
-        return
-    print("Get size function: passed")
+class TestSize(unittest.TestCase):
 
+    def test_SizeFor36(self):
+        x, y = category_rewritten.get_size_data(36)
+        self.assertEqual([x, y], [6, 6])
 
-def test_buttons():
-    category = category_rewritten.CategoryForm()
-    btn = category.create_button("test", 0, 0)
-    if not btn or btn.text() != "test":
-        print("Create button failed")
-        return
-    print("Btn create: passed")
+    def test_SizeFor35(self):
+        x, y = category_rewritten.get_size_data(35)
+        self.assertEqual([x, y], [7, 5])
 
+    def test_SizeFor21(self):
+        x, y = category_rewritten.get_size_data(21)
+        self.assertEqual([x, y], [5, 5])
 
-def test_main():
-    test_file_open()
-    test_get_size()
-    # test_buttons()
+"""class TestButtons(unittest.TestCase):
+
+    def test_BtnCreation(self):
+        game, _ = questiondata.create_game("Example.xlsx")
+        category = category_rewritten.CategoryForm(game)
+        btn = category.create_button("test", 0, 0)
+        self.assertEqual(btn.text(), "test")"""
 
 
 if __name__ == '__main__':
-    test_main()
+    unittest.main()
