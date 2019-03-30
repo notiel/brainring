1cd6db3f091a0564ca936a1122d31c99434f51b6
Julia Salnikova (Notiel) <agereth@gmail.com>
2019-03-31 02:33:38 +0300

    more typehinting. Time settings logic added:

--- settings.py
+++ settings.py
@@ -1,5 +1,6 @@
 import settings_ui
 import commanddata
+import questiondata
 from PyQt5 import QtWidgets
 from typing import List
 
@@ -9,52 +10,73 @@ class Settings(QtWidgets.QWidget, settings_ui.Ui_Settings):
     def __init__(self, commands):
         super().__init__()
         self.setupUi(self)
+        self.SpinBefore.setValue(questiondata.time_low_threshold)
+        self.SpinLength.setValue(questiondata.question_time)
         self.commanddata: commanddata.Commands = commands
-        self.checklist = [self.CB1, self.CB2, self.CB3, self.CB4, self.CB5, self.CB6, self.CB7, self.CB8, self.CB9,
-                          self.CB10, self.CB11, self.CB11, self.CB12, self.CB13, self.CB14, self.CB15, self.CB16]
-        self.btnlist = [self.Btn1, self.Btn2, self.Btn3, self.Btn4, self.Btn5, self.Btn6, self.Btn7, self.Btn8,
-                        self.Btn9, self.Btn10, self.Btn11, self.Btn12, self.Btn13, self.Btn14, self.Btn15, self.Btn16]
-        self.CBlist = [self.CBButton1, self.CBButton2, self.CBButton3, self.CBButton4, self.CBButton5,
-                       self.CBButton6, self.CBButton7, self.CBButton8, self.CBButton9, self.CBButton10, self.CBButton11,
-                       self.CBButton12, self.CBButton13, self.CBButton14, self.CBButton15, self.CBButton16]
-        self.CBlabels = list()
+        self.checklist: List[QtWidgets.QCheckBox] = [self.CB1, self.CB2, self.CB3, self.CB4, self.CB5, self.CB6,
+                                                     self.CB7, self.CB8, self.CB9, self.CB10, self.CB11, self.CB11,
+                                                     self.CB12, self.CB13, self.CB14, self.CB15, self.CB16]
+        self.btnlist: List[QtWidgets.QPushButton] = [self.Btn1, self.Btn2, self.Btn3, self.Btn4, self.Btn5, self.Btn6,
+                                                     self.Btn7, self.Btn8, self.Btn9, self.Btn10, self.Btn11,
+                                                     self.Btn12, self.Btn13, self.Btn14, self.Btn15, self.Btn16]
+        self.CBlist: List[QtWidgets.QComboBox] = [self.CBButton1, self.CBButton2, self.CBButton3, self.CBButton4,
+                                                  self.CBButton5, self.CBButton6, self.CBButton7, self.CBButton8,
+                                                  self.CBButton9, self.CBButton10, self.CBButton11, self.CBButton12,
+                                                  self.CBButton13, self.CBButton14, self.CBButton15, self.CBButton16]
+        self.CBlabels: List[str] = list()
         self.initUi()
 
     def initUi(self):
-
         for CB in self.checklist:
             CB.stateChanged.connect(self.CommandActivated)
         for CB in self.CBlist:
-            caption = "Кнопка %i" % (self.CBlist.index(CB) + 1)
+            caption:str = "Кнопка %i" % (self.CBlist.index(CB) + 1)
             CB.setCurrentText(caption)
             self.CBlabels.append(caption)
             CB.currentTextChanged.connect(self.ButtonSelected)
+        self.SpinLength.valueChanged.connect(self.QuestionTimeChanged)
 
     def CommandActivated(self):
         """
-        activates
+        activates and deactivates corresponging command
         :return:
         """
-        CB = self.sender()
-        i = self.checklist.index(CB)
-        if CB.isChecked():
-            self.btnlist[i].setEnabled(True)
-            self.CBlist[i].setEnabled(True)
-            self.commanddata.commands[i].available = True
-        else:
-            self.btnlist[i].setEnabled(False)
-            self.CBlist[i].setEnabled(False)
-            self.commanddata.commands[i].available = False
+        CB: QtWidgets.QCheckBox = self.sender()
+        i: int = self.checklist.index(CB)
+        status: bool = CB.isChecked()
+        self.btnlist[i].setEnabled(status)
+        self.CBlist[i].setEnabled(status)
+        self.commanddata.commands[i].available = status
 
     def ButtonSelected(self):
+        """
+        selects button for command (each command must have unique button id)
+        :return:
+        """
         sender: QtWidgets.QComboBox = self.sender()
         current: str = sender.currentText()
         self.commanddata.commands[self.CBlist.index(sender)].button_id = int(current.replace("Кнопка ", ""))
+        # find absent button and set duplicate to absent
         current_labels: List[str] = [CB.currentText() for CB in self.CBlist]
         set_current = set(current_labels)
         if len(set_current) != len(self.CBlabels):
-            absent = list(set(self.CBlabels) - set_current)[0]
+            absent: str = list(set(self.CBlabels) - set_current)[0]
             for CB in self.CBlist:
                 if CB != sender and CB.currentText() == current:
                     CB.setCurrentText(absent)
                     self.commanddata.commands[self.CBlist.index(CB)].button_id = int(absent.replace("Кнопка ", ""))
+
+    def QuestionTimeChanged(self):
+        """
+        change question length
+        :return:
+        """
+        self.SpinBefore.setMaximum(self.SpinLength.value())
+        questiondata.question_time = self.SpinLength.value()
+
+    def TimeLowThresholdChanged(self):
+        """
+        change time threshold for giving signal that time is almost ended
+        :return:
+        """
+        questiondata.time_low_threshold = self.SpinBefore.value()
