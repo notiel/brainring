e35e6e868302faad0f0b4c6c1886d673e5517afb
Julia Salnikova (Notiel) <agereth@gmail.com>
2019-04-03 01:41:39 +0300

    bug in checkbox list fixed

--- settings.py
+++ settings.py
@@ -14,8 +14,8 @@ class Settings(QtWidgets.QWidget, settings_ui.Ui_Settings):
         self.SpinLength.setValue(questiondata.question_time)
         self.commanddata: commanddata.CommandTableModel = commandmodel
         self.checklist: List[QtWidgets.QCheckBox] = [self.CB1, self.CB2, self.CB3, self.CB4, self.CB5, self.CB6,
-                                                     self.CB7, self.CB8, self.CB9, self.CB10, self.CB11, self.CB11,
-                                                     self.CB12, self.CB13, self.CB14, self.CB15, self.CB16]
+                                                     self.CB7, self.CB8, self.CB9, self.CB10, self.CB11, self.CB12,
+                                                     self.CB13, self.CB14, self.CB15, self.CB16]
         self.btnlist: List[QtWidgets.QPushButton] = [self.Btn1, self.Btn2, self.Btn3, self.Btn4, self.Btn5, self.Btn6,
                                                      self.Btn7, self.Btn8, self.Btn9, self.Btn10, self.Btn11,
                                                      self.Btn12, self.Btn13, self.Btn14, self.Btn15, self.Btn16]
