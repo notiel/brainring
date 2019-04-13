bbd6bef4cf333d2e12709b71fc8af94dbf3f1089
Julia Salnikova (Notiel) <agereth@gmail.com>
2019-04-13 18:53:31 +0300

    file for common functions added

---
+++ common_functions.py
@@ -0,0 +1,15 @@
+from PyQt5 import QtWidgets
+
+
+def error_message(text):
+    """
+    shows error window with text
+    :param text: error text
+    :return:
+    """
+    error = QtWidgets.QMessageBox()
+    error.setIcon(QtWidgets.QMessageBox.Critical)
+    error.setText(text)
+    error.setWindowTitle('Ошибка!')
+    error.setStandardButtons(QtWidgets.QMessageBox.Ok)
+    error.exec_()
